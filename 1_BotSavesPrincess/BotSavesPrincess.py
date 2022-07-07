efe29b3399ecdb3e21455531eb83fbbe1e01e9b1
Aline Almeida <31647699+AlmeidaAlin3@users.noreply.github.com>
2022-07-07 15:34:28 -0300

    Update BotSavesPrincess.py

--- 1_BotSavesPrincess/BotSavesPrincess.py
+++ 1_BotSavesPrincess/BotSavesPrincess.py
@@ -1,1 +1,36 @@
+#!/usr/bin/python
 
+def get_position(who): #who is "p" or "m"        
+    for row in range(len(grid)):
+        col = grid[row].find(who)
+        if col!=-1:
+            return(row, col)
+           
+def displayPathtoPrincess(n, grid):      
+    row_robot, col_robot = get_position("m")
+    row_princess, col_princess = get_position("p")
+
+    while(col_robot!=col_princess):
+        if col_robot>col_princess:
+            print("LEFT")
+            col_robot-=1
+
+        elif col_robot<col_princess:
+            print("RIGHT")
+            col_robot+=1
+        
+    while(row_robot!=row_princess):
+        if row_robot<row_princess:
+                print("DOWN")
+                row_robot+=1
+
+        elif row_robot>row_princess:
+                print("UP")
+                row_robot-=1
+
+m = int(input())
+grid = []
+for i in range(0, m):
+    grid.append(input().strip())
+
+displayPathtoPrincess(m, grid)
